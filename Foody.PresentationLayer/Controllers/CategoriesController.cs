8b5c3793c3d51d471de4a540368206900f2a9f52
Furkan Keleş <furkan.keles.8245@hotmail.com>
2025-01-12 16:21:53 +0300

    Database operations have been updated and the admin panel has been modified.

---
+++ Foody.PresentationLayer/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private readonly ICategorySevice _categorySevice;
+
+		public CategoriesController(ICategorySevice categorySevice)
+		{
+			_categorySevice = categorySevice;
+		}
+
+		public IActionResult CategoryList()
+		{
+			var values = _categorySevice.TGetAll();
+			return View(values);
+		}
+	}
+}
