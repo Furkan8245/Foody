fa71cdc211b95f334e60b650f2e781da173655f6
Furkan Keleş <furkan.keles.8245@hotmail.com>
2025-01-21 18:31:06 +0300

    Entity Specific Method (Product List With Category)

--- Foody.PresentationLayer/Program.cs
+++ Foody.PresentationLayer/Program.cs
@@ -12,6 +12,9 @@ builder.Services.AddDbContext<FoodyContext>();
 builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
 builder.Services.AddScoped<ICategorySevice, CategoryManager>();
 
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+
 builder.Services.AddControllersWithViews();
 
 
