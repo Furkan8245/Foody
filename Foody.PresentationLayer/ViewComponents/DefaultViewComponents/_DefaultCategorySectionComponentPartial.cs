f5f0abbef1c1f544c400488e86967d4d81341b36
Furkan Keleş <furkan.keles.8245@hotmail.com>
2025-01-28 19:17:33 +0300

    Category razor has been created.

---
+++ Foody.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultCategorySectionComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultCategorySectionComponentPartial:ViewComponent
+    {
+        private readonly ICategorySevice _categorySevice;
+
+        public _DefaultCategorySectionComponentPartial(ICategorySevice categorySevice)
+        {
+            _categorySevice = categorySevice;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values= _categorySevice.TGetAll();
+            return View(values);
+        }
+    }
+}
